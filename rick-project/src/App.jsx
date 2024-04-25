46f0b3c330f3d95f4eba1fa03640e4332a3433d0
TimDzen <arktimur031@gmail.com>
2024-04-25 15:17:32 +0300

    Initial commit

---
+++ rick-project/src/App.jsx
@@ -0,0 +1,20 @@
+
+import { Rick } from "./components/Rick";
+import { EpisodeList } from "./components/presentational/EpisodeList";
+import { LocationResidentsList } from "./components/presentational/LocationResidentsList";
+
+
+function App() {
+  return (
+    <>
+      <h1>Episodes:</h1>
+      <EpisodeList />
+      <h1>Locations:</h1>
+
+      <LocationResidentsList />
+
+    </>
+  );
+}
+
+export default App;
---
+++ rick-project/src/components/presentational/LocationResidentsList.jsx
@@ -0,0 +1,28 @@
+
+import {Collapse} from "./Collapce.jsx";
+import { CharacterList } from "./CharacterList";
+import { useLocations } from "../hooks/useLocations";
+
+export const LocationResidentsList = () => {
+  const { locations } = useLocations();
+  return (
+    <div>
+      {locations.map((location) => (
+        <Collapse
+          key={location.id}
+          className="locations-container"
+          title={location.name + ":" + location.type + " with " + location.residents.length + " resident(s)."}
+          content={location.residents == false ? "No residents here..." : <CharacterList
+                    ids={
+                        location.residents.map((character) => {
+                        const id = character.split("/").pop();
+                        return id;
+                    })}
+                />}
+
+
+        />
+      ))}
+    </div>
+  );
+};
